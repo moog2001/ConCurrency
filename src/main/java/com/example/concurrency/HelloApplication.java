a167fff733be05a020cb43f5602a7986f36cd027
moog2001 <moogforhire@gmail.com>
2022-05-13 00:56:44 +0800

    added timer and things

--- src/main/java/com/example/concurrency/HelloApplication.java
+++ src/main/java/com/example/concurrency/HelloApplication.java
@@ -17,7 +17,7 @@ public class HelloApplication extends Application {
         stage.show();
 
         HelloController controller = fxmlLoader.getController();
-        controller.init();
+        controller.init(stage);
     }
 
 //    for (Node node : parent.getChildrenUnmodifiable()) {
--- src/main/java/com/example/concurrency/HelloController.java
+++ src/main/java/com/example/concurrency/HelloController.java
@@ -1,20 +1,30 @@
 package com.example.concurrency;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.Timer;
 
 public class HelloController {
 
     @FXML
     private HBox conTimer;
+
+    @FXML
+    private Label lblTimer;
 
     @FXML
     private ProgressBar pbM0;
@@ -88,10 +98,11 @@ public class HelloController {
     @FXML
     private HBox conM0;
 
-    ArrayList<HBox> arrayListM = new ArrayList();
-    ArrayList<ProgressBar> arrayListBars = new ArrayList();
-    ArrayList<Task> tasks = new ArrayList();
-
+    ArrayList<HBox> arrayListM = new ArrayList<>();
+    ArrayList<ProgressBar> arrayListBars = new ArrayList<>();
+    ArrayList<Task<Integer>> tasks = new ArrayList<Task<Integer>>();
+    ArrayList<Integer> totals = new ArrayList();
+    ArrayList<Integer> results = new ArrayList();
 
     @FXML
     private VBox root;
@@ -100,8 +111,10 @@ public class HelloController {
     int total = 0;
 
     private HelloController self;
+    Timer timer = new Timer();
+    Integer timeSeconds = 3;
 
-    public void init() {
+    public void init(Stage parent) {
         self = this;
         for (Node node : root.getChildrenUnmodifiable()) {
             if (node instanceof HBox) {
@@ -114,25 +127,87 @@ public class HelloController {
         }
 
 
+        Timeline timeline = new Timeline();
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        // KeyFrame event handler
+        timeline.getKeyFrames().add(
+                new KeyFrame(Duration.seconds(1),
+                        (EventHandler<ActionEvent>) event -> {
+                            timeSeconds--;
+                            // update timerLabel
+                            lblTimer.setText(
+                                    timeSeconds.toString());
+                            if (timeSeconds <= 0) {
+                                int index = 0;
+                                tasks.forEach(n -> {
+                                    n.cancel();
+                                });
+                                for (HBox hBox : arrayListM) {
+                                    Label test = new Label(String.valueOf(results.get(index)) + "/" + String.valueOf(totals.get(index)));
+                                    hBox.getChildren().add(test);
+                                    index++;
+                                }
+                                root.setMinWidth(300);
+                                parent.setMinWidth(300);
+                                timeline.stop();
+
+                                int total = 0;
+                                for(int a :totals){
+                                    total= a+total;
+                                }
+                                int result = 0;
+                                for(int a :results){
+                                    result= a+result;
+                                }
+
+                                Label lblM0 = new Label(String.valueOf(result) + "/" + String.valueOf(total));
+                                conM0.getChildren().add(lblM0);
+                            }
+                        }));
+
+
+        timeline.playFromStart();
+
+
         Random r = new Random();
 
-        arrayListM.forEach((node) -> {
+
+        int index = 0;
+        for (int i = 0; i < 23; i++) {
+            results.add(0);
+        }
+        for (HBox node : arrayListM) {
             Task task = new Task<Integer>() {
+                Integer result;
+                Integer indexTask;
+
+                private Task init(int indexInput) {
+                    indexTask = indexInput;
+                    return this;
+                }
+
+                @Override
+                protected void cancelled() {
+                    super.cancelled();
+                    results.set(indexTask, result);
+                }
+
+
                 @Override
                 public Integer call() {
                     final int max = r.nextInt(50000);
+                    totals.add(max);
                     boolean done = false;
                     if (!done) {
                         total = total + max;
                         done = true;
                     }
-                    int i;
-                    for (i = 1; i <= max; i++) {
+                    for (int i = 1; i <= max; i++) {
                         if (isCancelled()) {
                             break;
                         }
                         updateProgress(i, max);
-
+                        result = i;
                         try {
                             Thread.sleep(r.nextInt(4));
                         } catch (InterruptedException interrupted) {
@@ -141,13 +216,10 @@ public class HelloController {
                             }
                         }
                     }
-                    return null;
+                    return result;
                 }
-            };
-
+            }.init(index);
             tasks.add(task);
-
-
             for (Node children : node.getChildrenUnmodifiable()) {
                 if (children instanceof ProgressBar) {
                     arrayListBars.add((ProgressBar) children);
@@ -156,11 +228,10 @@ public class HelloController {
                 }
                 new Thread(task).start();
             }
+            index++;
+        }
 
-
-        });
-
-        Task taskM0 = new Task<Void>() {
+        Task<Void> taskM0 = new Task<Void>() {
             @Override
             protected Void call() throws Exception {
                 double progress = 0;
